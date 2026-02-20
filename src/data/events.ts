c12ad22758993514ab1d00351db3f5a5d379dfe3
Surbhi <65139581+hellosurbhi@users.noreply.github.com>
2026-02-20 12:20:49 -0500

    feat: add San Diego Eventbrite ticket link

--- src/data/events.ts
+++ src/data/events.ts
@@ -16,7 +16,7 @@ export const events: EventEntry[] = [
     date: "Mar 7",
     city: "San Diego",
     numeral: "II",
-    url: "#",
+    url: "https://www.eventbrite.com/e/garam-masala-dating-live-in-san-diego-tickets-1983622967694?lid=ipmjzd9i2ysd&utm_source=braze&utm_medium=ebml&utm_campaign=clpo_ceex_lcm_fad_mec_mc_mum_0_0_eventpublished&utm_term=Main_EventPublishedSubPaid_HeroSummary_other%20feature%20usage&utm_content=d83bc845-0544-4274-a9dd-e152eac1a1f7__699895f399367e11f0839776e4c4c33f__f605d558-54e9-4fbc-aef1-2c09bcef5c27",
   },
   {
     date: "Mar 15",

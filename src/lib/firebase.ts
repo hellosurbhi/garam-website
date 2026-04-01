fafb1dfe2e064a938ec7dad0c59ade383358078d
Surbhi <65139581+hellosurbhi@users.noreply.github.com>
2026-04-01 10:44:32 -0400

    updated firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY?.trim(),
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN?.trim(),
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID?.trim(),
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET?.trim(),
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID?.trim(),
-  appId: import.meta.env.VITE_FIREBASE_APP_ID?.trim(),
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY?.trim(),
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN?.trim(),
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID?.trim(),
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET?.trim(),
+  messagingSenderId:
+    import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID?.trim(),
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID?.trim(),
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
